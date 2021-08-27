f295426d516189773b3b49588828bce86d16eb0f
__________sofi____________(✧ω✧) <sofnayd@gmail.com>
2021-08-27 23:27:18 +0300

    project done

---
+++ main.py
@@ -0,0 +1,13 @@
+print("""
+scenario of a thriller scenario
+""")
+print(" *The night was horrible. Feeling depressive again... You walk into the bathroom to wash your face...* ")
+print("hella, welcome! my name is Mirror")
+print("I will repeat everything what you're saying.")
+stuff_to_repeat = input("why do you look so pale? let's play this little game! Say something: ")
+print("you said " + stuff_to_repeat)
+print("don't be so frightened. I'm your mirror image, this is what I suppose to do: repeat everything you're saying!")
+stuff_to_repeat1 = input("say something funny: ")
+print("hahahha you said " + stuff_to_repeat1 + "  I like it! Of course I do. We have the same humour, kinda...")
+stuff_to_repeat2 = input("now, say goodbye to me and go do something useful: ")
+print("you said " + stuff_to_repeat2 + " :-) don't forget to smile  :-) ")
